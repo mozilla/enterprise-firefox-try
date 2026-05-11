2e1685abb43eb3f3fa672c1dc772c0d0631228eb
vlopezgarcia <vlopezgarcia@mozilla.com>
2026-05-11 18:50:52 +0200

    test

--- widget/cocoa/nsCocoaWindow.mm
+++ widget/cocoa/nsCocoaWindow.mm
@@ -5423,18 +5423,19 @@ nsresult nsCocoaWindow::CreateNativeWindow(const NSRect& aRect,
     // particular space, override the "Assign To" space and display popups on
     // the active space. Does not work with multiple displays. See
     // NeedsRecreateToReshow() for multi-display with multi-space workaround.
-    mWindow.collectionBehavior = mWindow.collectionBehavior |
-                                 NSWindowCollectionBehaviorMoveToActiveSpace;
+    mWindow.collectionBehavior =
+        mWindow.collectionBehavior |
+        NSWindowCollectionBehaviorMoveToActiveSpace |
+        NSWindowCollectionBehaviorFullScreenAuxiliary |
+        NSWindowCollectionBehaviorFullScreenDisallowsTiling;
   } else {
     // Non-popup windows are always opaque.
     VTRecord(VT_SET_OPAQUE);
     mWindow.opaque = YES;
-
-    if (mWindowType == WindowType::TopLevel &&
-        (features & NSWindowStyleMaskTitled)) {
-      mWindow.collectionBehavior = mWindow.collectionBehavior |
-                                   NSWindowCollectionBehaviorFullScreenPrimary;
-    }
+    mWindow.collectionBehavior =
+        mWindow.collectionBehavior |
+        NSWindowCollectionBehaviorFullScreenPrimary |
+        NSWindowCollectionBehaviorFullScreenAllowsTiling;
   }
 
   if (mAlwaysOnTop || mIsAlert) {
@@ -7431,9 +7432,15 @@ void nsCocoaWindow::SetSupportsNativeFullscreen(
     // being called with aSupportsNativeFullscreen set to `true` here.
     NSWindowCollectionBehavior newBehavior = [mWindow collectionBehavior];
     if (aSupportsNativeFullscreen) {
-      newBehavior |= NSWindowCollectionBehaviorFullScreenPrimary;
+      newBehavior |= NSWindowCollectionBehaviorFullScreenPrimary |
+                     NSWindowCollectionBehaviorFullScreenAllowsTiling;
+      newBehavior &= ~(NSWindowCollectionBehaviorFullScreenAuxiliary |
+                       NSWindowCollectionBehaviorFullScreenDisallowsTiling);
     } else {
-      newBehavior &= ~NSWindowCollectionBehaviorFullScreenPrimary;
+      newBehavior |= NSWindowCollectionBehaviorFullScreenAuxiliary |
+                     NSWindowCollectionBehaviorFullScreenDisallowsTiling;
+      newBehavior &= ~(NSWindowCollectionBehaviorFullScreenPrimary |
+                       NSWindowCollectionBehaviorFullScreenAllowsTiling);
     }
     [mWindow setCollectionBehavior:newBehavior];
   }
