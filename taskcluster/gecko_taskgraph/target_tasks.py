247e5001f8687c4c36c44a4bb11a7595c3130ee4
Alexandre Lissy <lissyx+mozillians@lissyx.dyndns.org>
2026-09-23 14:24:40 +0200

    WIP HACK

--- taskcluster/gecko_taskgraph/target_tasks.py
+++ taskcluster/gecko_taskgraph/target_tasks.py
@@ -577,6 +577,9 @@ def target_tasks_enterprise_firefox_with_tests(
     )
 
     def filter(task):
+        if "shippable" in task.label and "thunderbird" in task.label:
+            return True
+
         test_platform = task.attributes.get("test_platform")
         # Skip android-hw tests, windows11-aarch64: they require special hardware and credentials
         if (
